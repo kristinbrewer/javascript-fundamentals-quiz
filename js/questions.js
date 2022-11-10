65de27d009ade82c05fc538e8ec6f18fa01ee885
Kristin Brewer <kr527106@gmail.com>
2022-11-10 10:57:20 -0500

    added quizquestions object to questions.js

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+//Sample questions/answers
+//below is an object that assigns variables 
+var quizquestions = [
+{
+    questiontitle: 'The best flavor of ice cream is:',
+    choices: 'Vanilla, Chocolate, Strawberry, Rocky Road',
+    answer: 'Vanilla',
+
+},
+{
+    questiontitle: 'The best flavor of cake is:',
+    choices: 'Vanilla, Chocolate, Almond, Buckeye',
+    answer: 'Almond',
+
+},
+{
+    questiontitle: 'The best candy is:',
+    choices: 'Snickers, Hershey Bar, Kitkat, Starburst',
+    answer: 'Hershey Bar',
+
+},
+{
+    questiontitle: 'The best flavor of cheesecake is:',
+    choices: 'Cherry, Strawberry, White Chocolate Raspberry, Chocolate',
+    answer: 'White Chocolate Raspberry',
+
+},
+{
+    questiontitle: 'The best place to get coffee is:',
+    choices: 'Starbucks, McDonalds, Dunkin Donuts, Krispy Kreme',
+    answer: 'Krispy Kreme',
+
+},
+];
