194b612c511758a84bc1948bb0b0590ae3c44cb4
Kristin Brewer <kr527106@gmail.com>
2022-11-15 19:10:43 -0500

    hidestart function complete and quiz start modified

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var startbutton = document.getElementById('start-button');
 var welcomescreen = document.body.querySelector("#welcome-screen");
-var quizquestions = document.body.querySelector("#quiz-questions");
+var quiz = document.getElementById('quiz-questions');
+var answers = document.body.querySelector("#answers");
 var endpage = document.body.querySelector("#end-page");
 
 var questionindex = 0;
@@ -10,33 +11,57 @@ var questionindex = 0;
 
 //when the start button is selected, the start screen disappears
 
+
 function hidestart () {  
+quiz === quizquestions;
 startbutton.setAttribute("class", "display");
 welcomescreen.setAttribute("class", "display");
-}
+quiz.removeAttribute("class");
 
+}
 startbutton.addEventListener("click", hidestart);
-//array for questions
-var questions = 
+
+
 //and the first question pops up 
 function quizstarts () {
-
-}
-
-
-
-
-//show random deck of questions
-//for loop
-
-
-//tally score
-
-//subtract 15 points 
-
-//enter initials 
-
-
-
-
-// Access toggle switch HTML element
+    //array for questions
+    var questionup = quizquestions[questionindex];
+    //question is updated 
+    var linequestion = document.getElementById('question');
+    linequestion.textContent = questionup.title;
+    
+    //clear answers
+    answers.innerHTML = '';
+    
+    //loop for choices
+    for (var i = 0; i < questionup.options.length; i++){
+    //created a new button
+    var options = questionup.options[i];
+    var getbutton = document.createElement('button');
+    getbutton.setAttribute('class', 'options');
+    getbutton.setAttribute('value', options);
+    
+    getbutton.textContent = i + 1 + '. ' + options;
+    
+    //on page
+    answers.appendChild(getbutton);
+      }
+    }
+    
+    
+    
+    //show random deck of questions
+    //for loop
+    
+    //tally score
+    
+    //subtract 15 points 
+    
+    //enter initials 
+    
+    
+    
+    // Access toggle switch HTML element
+    
+    
+    
